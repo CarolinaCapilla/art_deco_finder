5919dacf81a70a3b2f64a0b995accf872c93a6cb
CarolinaCapilla <carolina.capilla@gmail.com>
2020-08-01 11:18:48 +0100

    customised devise forms + added flashes (notice/alert)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :items, dependent: :destroy
-  validates :name, uniqueness: true
+  has_many :bookings
+
+  validates :name, uniqueness: true, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
