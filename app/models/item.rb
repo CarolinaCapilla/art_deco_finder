ea7107ab4281f2cccb4a57b7bbb57f6f2dd4192a
yazeed almusaybih <yazeedalmusaybih@gmail.com>
2020-08-01 12:09:40 +0100

    I have finished form item model an controller

---
+++ app/models/item.rb
@@ -0,0 +1,8 @@
+class Item < ApplicationRecord
+  belongs_to :user
+
+  validates :title, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
+  validates :category, inclusion: { in: ["Paintings", "Abstract", "Print", "Photo", "Drawings", "Decorative"]}
+end
