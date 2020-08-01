88c15e1dbe887689ab0a6bba6e6d58b4faecc91d
Felipe Cianciarulo <mfcianciarulo@gmail.com>
2020-08-01 14:45:42 +0100

    first routes implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :bookings
 
-  validates :name, uniqueness: true, presence: true
+  # validates :name, uniqueness: true, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
