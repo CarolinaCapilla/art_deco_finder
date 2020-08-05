6199274d08fda7407f24101684e241a546514ad5
CarolinaCapilla <carolina.capilla@gmail.com>
2020-08-05 11:12:45 +0100

    added date validation to booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,16 @@ class Booking < ApplicationRecord
   belongs_to :item
   belongs_to :user
 
-  validates :start_date, presence: true
+  validates :starts_at, :ends_at, presence: true
+  validate :ends_at_after_starts_at
+
+  private
+
+  def ends_at_after_starts_at
+    return if ends_at.blank? || starts_at.blank?
+
+    if ends_at < starts_at
+      errors.add(:ends_at, "must be after the start date")
+    end
+  end
 end
