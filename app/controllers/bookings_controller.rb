3c0371956c317e561c5dcd1a8f977a6cfa002345
CarolinaCapilla <carolina.capilla@gmail.com>
2020-08-05 11:36:51 +0100

    added all crud routes to bookings + bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,11 @@
 class BookingsController < ApplicationController
-  # def index
-  #   @bookings = Booking.all
-  # end
+  before_action :set_booking, except: [:index]
+
+  def index
+    @bookings = Booking.where(user_id: current_user.id)
+  end
+
+  def show; end
 
   def new
     @booking = Booking.new
@@ -9,10 +13,25 @@ class BookingsController < ApplicationController
 
   def create
     @booking = Booking.new(bookings_params)
+    @booking.item = @item
+    @booking.user = current_user
     if @booking.save
+      # set booking to true @booking.booked = true ?
+      @booking.update_attributes(booked: true)
       redirect_to booking_path(@booking), notice: "Booking succesfully created."
     else
       render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    @booking.update(booking_params)
+    if @booking.save
+      redirect_to experience_path(@experience)
+    else
+      render :edit
     end
   end
 
@@ -24,7 +43,11 @@ class BookingsController < ApplicationController
 
   private
 
+  def set_booking
+    @booking = Booking.find(params[:item_id])
+  end
+
   def bookings_params
-    params.require(:booking).permit(:start_date, :item_id, :user_id)
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
