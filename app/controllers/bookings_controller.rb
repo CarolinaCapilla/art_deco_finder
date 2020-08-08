abe5e1576bef13f3dd2612f39c78bb82a01b7b23
CarolinaCapilla <carolina.capilla@gmail.com>
2020-08-08 13:25:41 +0100

    fixed edit + destroy method on bookings + redirect

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,52 +1,53 @@
 class BookingsController < ApplicationController
-  before_action :set_item, except: [:index]
+  before_action :set_booking, only: [:show, :update, :edit, :destroy]
 
   def index
-    @bookings = Booking.where(user_id: current_user.id)
+    # @bookings = Booking.where(user_id: current_user.id)
+    @bookings = Booking.all
   end
 
-  def show
-    @booking = Booking.find(params[:id])
-  end
+  def show; end
 
   def new
+    @item = Item.find(params[:item_id])
     @booking = Booking.new
   end
 
   def create
     @booking = Booking.new(booking_params)
-    @booking.item = @item
+    @booking.item = Item.find(params[:item_id])
     @booking.user = current_user
     if @booking.save
       # set booking to true @booking.booked = true ?
-      @booking.update_attributes(booked: true)
-      redirect_to item_booking_path(@item, @booking), notice: "Booking successfully created."
+      # @booking.update_attributes(booked: true)
+      redirect_to booking_path(@booking), notice: "Booking successfully created."
     else
       render :new
     end
   end
 
-  def edit; end
+  def edit
+    @item = @booking.item
+  end
 
   def update
     @booking.update(booking_params)
     if @booking.save
-      redirect_to @item, notice: "Booking successfully updated."
+      redirect_to booking_path(@booking), notice: "Booking successfully updated."
     else
       render :edit
     end
   end
 
   def destroy
-    @booking = Booking.find(params[:id])
     @booking.destroy
-    redirect_to @item, notice: "Booking successfully destroyed."
+    redirect_to items_path, notice: "Booking successfully destroyed."
   end
 
   private
 
-  def set_item
-    @item = Item.find(params[:item_id])
+  def set_booking
+    @booking = Booking.find(params[:id])
   end
 
   def booking_params
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :items do
-    resources :bookings
+    resources :bookings, only: [:create, :new, :edit, :update]
   end
+  resources :bookings, only: [:show, :index, :destroy]
 end
